95377769b96b26af2412ecb74ece6a51cd524ca0
Cjhightower38 <Cjhightower38@gmail.com>
2021-05-31 15:39:40 -0400

    add fading colors to emphasize points

--- random_walk_visual.py
+++ random_walk_visual.py
@@ -11,11 +11,19 @@ while True:
     Make a random walk, and plot the points. Abbreviates the RandomWalk()
     and call fill_walk() to create and store the points generated.
     '''
+    
     rw = RandomWalk()
     rw.fill_walk()
 
-    # Scatters the x and y points with a point size of 15 and displays.
-    plt.scatter(rw.x_values, rw.y_values, s = 15)
+    '''
+    Scatters the x and y points with a point size of 15 and displays.
+    Store a list of numbers equal to the points in a walk in variable
+    (point_numbers)and remove the black outline around the points'
+    '''
+    
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c = point_numbers,
+        cmap = plt.cm.Reds, edgecolor = 'none', s = 15)
     plt.show()
     
     keep_running = input('Make another walk? (y/n): ')
