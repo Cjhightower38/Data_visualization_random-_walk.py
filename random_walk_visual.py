6f9cbf622b6dba60816c718af13d14b86997f9ad
Cjhightower38 <Cjhightower38@gmail.com>
2021-05-31 14:33:40 -0400

    add visual file to display random walk

---
+++ random_walk_visual.py
@@ -0,0 +1,16 @@
+# Tells Python which module to import and abbreviates it as plt.
+import matplotlib.pyplot as plt
+
+# Tells Python which folder and what class to import from that folder.
+from random_walk import RandomWalk
+
+'''
+Make a random walk, and plot the points. Abbreviates the RandomWalk()
+and call fill_walk() to create and store the points generated.
+'''
+rw = RandomWalk()
+rw.fill_walk()
+
+# Scatters the x and y points with a point size of 15 and displays.
+plt.scatter(rw.x_values, rw.y_values, s = 15)
+plt.show()
