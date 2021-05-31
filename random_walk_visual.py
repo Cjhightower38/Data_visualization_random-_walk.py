2fd798e68c05a8cd4929f0164ad2357883e6f716
Cjhightower38 <Cjhightower38@gmail.com>
2021-05-31 14:58:29 -0400

    add multiple random walks option

--- random_walk_visual.py
+++ random_walk_visual.py
@@ -4,13 +4,20 @@ import matplotlib.pyplot as plt
 # Tells Python which folder and what class to import from that folder.
 from random_walk import RandomWalk
 
-'''
-Make a random walk, and plot the points. Abbreviates the RandomWalk()
-and call fill_walk() to create and store the points generated.
-'''
-rw = RandomWalk()
-rw.fill_walk()
+# Keep making new walks, as long as the program is active.
+while True:
+	
+    '''
+    Make a random walk, and plot the points. Abbreviates the RandomWalk()
+    and call fill_walk() to create and store the points generated.
+    '''
+    rw = RandomWalk()
+    rw.fill_walk()
 
-# Scatters the x and y points with a point size of 15 and displays.
-plt.scatter(rw.x_values, rw.y_values, s = 15)
-plt.show()
+    # Scatters the x and y points with a point size of 15 and displays.
+    plt.scatter(rw.x_values, rw.y_values, s = 15)
+    plt.show()
+    
+    keep_running = input('Make another walk? (y/n): ')
+    if keep_running == 'n':
+        break
